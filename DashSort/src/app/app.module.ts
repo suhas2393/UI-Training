06b7584c1a8c57c2ce3ee061cb9c9cbe0555be3c
Suhas R K <srk@extremenetworks.com>
2023-10-06 11:30:52 +0530

    added login,register component

--- DashSort/src/app/app.module.ts
+++ DashSort/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
